7e74576a6de647a9a7fb642554a910fb4fa0286f
dorogakmillionuru-spec <dorogakmillionu.ru@gmail.com>
2026-01-25 20:04:19 +0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,3 +1,6 @@
 module.exports = function handler(req, res) {
-  res.status(200).send("Толик жив");
+  const { message } = req.body || {}
+  res.status(200).json({
+    reply: "Толик слышу: " + (message || "ничего не сказали")
+  })
 }
